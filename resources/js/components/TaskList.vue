ce6e754cefeee2ff0940a6a3b0116f3c72185753
Arsenio Ambayec <arambayec@iskolarngbayan.pup.edu.ph>
2023-08-01 16:47:17 +0800

    insert: sorting and edit bug fix

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -9,22 +9,49 @@
     <table v-else class="table">
       <thead>
         <tr>
-          <th scope="col">Task #</th>
-          <th scope="col">Title</th>
-          <th scope="col">Description</th>
-          <th scope="col">Status</th>
+          <th scope="col">
+            <span @click="sortBy('id')" style="cursor: pointer;">
+              Task #
+              <i :class="['fas', sortKey === 'id' ? (sortDirection === 1 ? 'fa-chevron-down' : 'fa-chevron-up') : 'fa-sort']"></i>
+            </span>
+          </th>
+          <th scope="col">
+            <span @click="sortBy('title')" style="cursor: pointer;">
+              Title
+              <i :class="['fas', sortKey === 'title' ? (sortDirection === 1 ? 'fa-chevron-down' : 'fa-chevron-up') : 'fa-sort']"></i>
+            </span>
+          </th>
+          <th scope="col">
+            <span @click="sortBy('description')" style="cursor: pointer;">
+              Description
+              <i :class="['fas', sortKey === 'description' ? (sortDirection === 1 ? 'fa-chevron-down' : 'fa-chevron-up') : 'fa-sort']"></i>
+            </span>
+          </th>
+          <th scope="col">
+            <span @click="sortBy('status')" style="cursor: pointer;">
+              Status
+              <i :class="['fas', sortKey === 'status' ? (sortDirection === 1 ? 'fa-chevron-down' : 'fa-chevron-up') : 'fa-sort']"></i>
+            </span>
+          </th>
+          <th scope="col">
+            <span @click="sortBy('created_at')" style="cursor: pointer;">
+              Date Created
+              <i :class="['fas', sortKey === 'created_at' ? (sortDirection === 1 ? 'fa-chevron-down' : 'fa-chevron-up') : 'fa-sort']"></i>
+            </span>
+          </th>
           <th scope="col">Uploaded File</th>
           <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="task in tasks" :key="task.id">
+        <tr v-for="task in sortedTasks" :key="task.id">
           <td>
               {{ task.id }}
           </td>
           <td>{{ task.title }}</td>
           <td>{{ task.description }}</td>
           <td>{{ task.status }}</td>
+          <td>{{ task.created_at }}</td>
           <td>
             <!-- Show the file name if available -->
             <span v-if="task.file_name">
@@ -109,7 +136,7 @@
 </div>
 
 <!-- Edit Task Modal -->
-<div v-if="showEditModal" class="modal">
+<div v-if="showEditModal && taskToEdit" class="modal">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header text-white">
@@ -157,6 +184,10 @@ export default {
       showEditModal: false, // For the edit modal
       taskToView: null, // For the view modal
       taskToEdit: null, // For the edit modal
+      sortKey: null,
+      sortDirection: 1,
+      taskBeingEdited: null,
+      
     };
   },
   computed: {
@@ -169,7 +200,26 @@ export default {
         // When the local tasksList is updated, emit an event to update the parent component
         this.$emit('update-tasks', updatedTasks);
       }
-    }
+    },
+
+    sortedTasks() {
+      if (!this.sortKey) return this.tasksList;
+
+      // Create a copy of the tasks list to avoid modifying the original data
+      const tasks = [...this.tasksList];
+
+      // Sort the tasks based on the sortKey and sortDirection
+      tasks.sort((a, b) => {
+        const keyA = this.getKeyValue(a, this.sortKey);
+        const keyB = this.getKeyValue(b, this.sortKey);
+
+        if (keyA < keyB) return -1 * this.sortDirection;
+        if (keyA > keyB) return 1 * this.sortDirection;
+        return 0;
+      });
+
+      return tasks;
+    },
   },
 
   mounted() {
@@ -218,16 +268,22 @@ export default {
     },
 
     async updateTask() {
-    // Call the update endpoint using Axios
-    try {
-      await axios.put(`/api/tasks/${this.taskToEdit.id}`, this.taskToEdit);
-      console.log('Task updated successfully');
-      // Set showEditModal to false to close the modal after update
-      this.showEditModal = false;
-    } catch (error) {
-      console.error('Error updating task:', error);
-    }
-  },
+      // Call the update endpoint using Axios
+      try {
+        await axios.put(`/api/tasks/${this.taskToEdit.id}`, this.taskToEdit);
+        console.log('Task updated successfully');
+
+        // Update the tasks array with the edited task data
+        this.tasksList = this.tasksList.map((task) =>
+          task.id === this.taskToEdit.id ? this.taskToEdit : task
+        );
+
+        // Close the modal
+        this.showEditModal = false;
+      } catch (error) {
+        console.error('Error updating task:', error);
+      }
+    },
 
     async onTaskCreated(newTask) {
       try {
@@ -262,9 +318,34 @@ export default {
         this.showViewModal = true;
       },
       toggleEditModal(task) {
-        this.taskToEdit = task;
+          // Create a deep copy of the task being edited and store it in taskBeingEdited
+        this.taskToEdit = { ...task };
+        // Show the edit modal
         this.showEditModal = true;
       },
+
+      getKeyValue(obj, key) {
+      const keys = key.split('.');
+      let value = obj;
+
+      for (const k of keys) {
+        value = value[k];
+      }
+
+      return value;
+    },
+
+    // Method to trigger sorting
+    sortBy(key) {
+      if (this.sortKey === key) {
+        // Toggle the sort direction if the same key is clicked
+        this.sortDirection *= -1;
+      } else {
+        // Set the new sort key and default to ascending direction
+        this.sortKey = key;
+        this.sortDirection = 1;
+      }
+    },
   },
   created() {
     console.log('Tasks:', this.tasks); // Add this line to check the tasks data
